c3dbb54e7b9b3caf2edf5dddbc8ff71f599c1eb7
Cloudbop <boppincolin@gmail.com>
2021-03-16 19:30:45 +0000

    added <Head> to /index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import { getFeaturedEvents } from "../helpers/firebase-api";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 function HomePage(props) {
   const { featuredEvents } = props;
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
