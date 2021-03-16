c8e8037f4c1f0ceaf588003dc119f7b65773aa44
Cloudbop <boppincolin@gmail.com>
2021-03-16 18:07:51 +0000

    added SSR+SSG for pre-rendered evtns

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,18 +1,11 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from "react";
+import { getEventById, getAllEvents } from "../../helpers/firebase-api";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
-import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogistics from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content';
-import ErrorAlert from '../../components/ui/error-alert';
-
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
+function EventDetailPage({ event }) {
   if (!event) {
     return (
       <ErrorAlert>
@@ -35,6 +28,33 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  // get event id from context / url --> remember same regex as filename [eventId]
+  const eventId = context.params.eventId;
+  //
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      event
+    },
+    revalidate: 60 * 60
+  };
+}
+
+// tell nextjs what paths to pre-render otherwise it will be looking for a potnetiall infintie amount
+export async function getStaticPaths() {
+  const allEvents = await getAllEvents();
+  // create array of objects wth IDs
+  const eventIdPaths = allEvents.map(evt => ({ params: { eventId: evt.id } }));
+  // pre-render these paths
+  return {
+    paths: eventIdPaths,
+    // false we know ahead of time all of these pages
+    fallback: false
+  };
 }
 
 export default EventDetailPage;
