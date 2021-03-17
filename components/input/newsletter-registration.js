5526154bd00581406ff3ead26fdf7e2664d71edb
Cloudbop <boppincolin@gmail.com>
2021-03-17 14:07:43 +0000

    implmented notification via context

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,13 +1,18 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 import classes from "./newsletter-registration.module.css";
-
+import NotificationContext from "../../store/notification-context";
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
-
     const userEmail = emailInputRef.current.value;
+    notificationCtx.showNotification({
+      title: "Signing up!",
+      message: `Registering email ${userEmail} to newsletter`,
+      status: "pending"
+    });
     // TODO: validate input
     fetch("/api/newsletter", {
       method: "POST",
@@ -16,8 +21,31 @@ function NewsletterRegistration() {
         "Content-Type": "application/json"
       }
     })
-      .then(res => res.json())
-      .then(data => console.log(`data`, data));
+      .then(res => {
+        // remember, failed response from the server won't register in te catch block below
+        if (res.ok) {
+          return res.json();
+        }
+        // force it to throw error - this will trigger catch() below
+        return res.json().then(data => {
+          throw new Error(data.message || "something went wrong");
+        });
+      })
+      .then(data => {
+        notificationCtx.showNotification({
+          title: "Signed up!",
+          message: `${userEmail} is registered to newsletter`,
+          status: "success"
+        });
+      })
+      .catch(err => {
+        notificationCtx.showNotification({
+          title: "Failed to sign up!",
+          message:
+            err.message || `Failed to register ${userEmail} to newsletter`,
+          status: "error"
+        });
+      });
   }
 
   return (
