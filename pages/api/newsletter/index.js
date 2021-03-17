8f4a824cee7d5217390abfc8dfbc691fb0140aa5
Cloudbop <boppincolin@gmail.com>
2021-03-17 02:25:46 +0000

    added error detection

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,5 +1,19 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabse() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.mongouser}:${process.env.mongopw}@cluster0.61wo8.mongodb.net/nextjs-events-newsletter?retryWrites=true&w=majority`
+  );
+  return client;
+}
+
+async function insertDocument(client, document) {
+  // open db connection
+  const db = client.db();
+  // insert document into collection
+  await db.collection("emails").insertOne(document);
+}
+
 async function handler(req, res) {
   //
   if (req.method === "POST") {
@@ -10,18 +24,28 @@ async function handler(req, res) {
       res.status(422).json({ message: "Invalid email address" });
       return;
     }
+    let client;
+    try {
+      client = await connectDatabse();
+    } catch (error) {
+      console.log(`connectDatabase()-failed`);
+      console.log(error);
+      res.status(500).json({ message: "Connecting to the database failed" });
+      return;
+    }
 
-    console.log(userEmail);
-
-    const client = await MongoClient.connect(
-      `mongodb+srv://${process.env.mongouser}:${process.env.mongopw}@cluster0.61wo8.mongodb.net/nextjs-events-newsletter?retryWrites=true&w=majority`
-    );
-    // open db connection
-    const db = client.db();
-    // insert document into collection
-    await db.collection("emails").insertOne({ email: userEmail });
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      console.log(`insertDocument()-failed`);
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: "Inserting data into the database failed" });
+      return;
+    }
     //
-    client.close();
 
     res.status(201).json({ message: "Signed up!" });
   }
